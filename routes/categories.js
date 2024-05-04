a082b62f7512ac4f804863caf8f861cf8123253c
kronseld <veilikiygeolog@yandex.ru>
2024-05-04 20:28:22 +0400

    Проделана работа за пятую неделю обучения. Сайту добавлены возможности админки.

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,25 @@
 const categoriesRouter = require('express').Router();
-const findAllCategories = require('../middlewares/categories');
-const sendAllCategories = require('../controllers/categories');
+const { 
+    findAllCategories, 
+    findCategoryById, 
+    createCategory, 
+    updateCategory, 
+    deleteCategory,
+    checkIsCategoryExists,
+    checkEmptyName
+} = require('../middlewares/categories');
+const { 
+    sendAllCategories, 
+    sendCategoryById, 
+    sendCreatedCategory, 
+    sendUpdatedCategory, 
+    sendDeletedCat 
+} = require('../controllers/categories');
 
 categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
+categoriesRouter.get('/categories/:id', findCategoryById, sendCategoryById);
+categoriesRouter.post('/categories', findAllCategories, checkIsCategoryExists, checkEmptyName, createCategory, sendCreatedCategory);
+categoriesRouter.put('/categories/:id', findCategoryById, checkIsCategoryExists, checkEmptyName, updateCategory, sendUpdatedCategory);
+categoriesRouter.delete('/categories/:id', findCategoryById, deleteCategory, sendDeletedCat);
 
 module.exports = categoriesRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,26 @@
 const usersRouter = require('express').Router();
-const findAllUsers = require('../middlewares/users');
-const sendAllUsers = require('../controllers/users');
+const { 
+    findAllUsers, 
+    findUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser, 
+    checkEmptyNameAndEmail,
+    checkEmptyNameAndEmailAndPass,
+    checkIsUserExists 
+} = require('../middlewares/users');
+const { 
+    sendAllUsers, 
+    sendUserById, 
+    sendCreatedUser, 
+    sendUpdatedUser, 
+    sendDeletedUser 
+} = require('../controllers/users');
 
 usersRouter.get('/users', findAllUsers, sendAllUsers);
+usersRouter.get('/users/:id', findUserById, sendUserById);
+usersRouter.post('/users', findAllUsers, checkIsUserExists, checkEmptyNameAndEmailAndPass,  createUser, sendCreatedUser);
+usersRouter.put('/users/:id', findUserById, checkIsUserExists, checkEmptyNameAndEmail, updateUser, sendUpdatedUser);
+usersRouter.delete('/users/:id', findUserById, deleteUser, sendDeletedUser);
 
 module.exports = usersRouter;
