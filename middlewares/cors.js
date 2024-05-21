b90da281c1b33925f3cef280b07d4be565d2d47a
kronseld <veilikiygeolog@yandex.ru>
2024-05-21 20:35:24 +0400

    modified cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ["http://localhost:3000", "http://localhost:3001"];
+const allowedCors = ["http://xxx-games-xxx.nomoredomainswork.ru", "http://bestdomainever.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
